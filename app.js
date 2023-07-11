5e0d33a71bacde5cd5468c4d2e2094499d6d1a97
sijadkhan1678 <sijadkhan1678@gmail.com>
2023-07-11 21:46:41 +0500

    Code Refactored

--- app.js
+++ app.js
@@ -1,46 +1,47 @@
 
 // Get DOM Elements
 const slides = document.querySelectorAll('.slides');
-const btn1 = document.querySelector('.btn1')
-const btn2 = document.querySelector('.btn2')
-const btn3 = document.querySelector('.btn3')
+const btn1 = document.querySelector('.btn0')
+const btn2 = document.querySelector('.btn1')
+const btn3 = document.querySelector('.btn2')
+const slideNavigators = document.querySelectorAll('.slideNavigator')
 
 
 btn1.addEventListener('click', function (e) {
 
-    slides[2].style.transform = 'translateX(0%)';
-    slides[1].style.transform = 'translateX(0%)';
-    slides[0].style.transform = 'translateX(0%)';
-    btn1.style.backgroundColor = '#66004d';
-    btn3.style.backgroundColor = ''
-    btn2.style.backgroundColor = '';
+    let slideCounter = e.target.className.slice(18, 19);
+
+    slides.forEach(slide => slide.style.transform = `translateX(-${slideCounter * 100}%)`)
+
+    slideNavigators.forEach((slideNavigator, index) => {
+
+        slideNavigator.style.backgroundColor = index == slideCounter ? '#66004d' : ''
+    })
 
 })
 
 btn2.addEventListener('click', function (e) {
 
-    slides[0].style.transform = 'translateX(-100%)';
-    slides[1].style.transform = 'translateX(-100%)';
-    slides[2].style.transform = 'translateX(-100%)';
-    btn2.style.backgroundColor = '#66004d';
-    btn3.style.backgroundColor = ''
-    btn1.style.backgroundColor = '';
-        
+    let slideCounter = e.target.className.slice(18, 19);
+
+    slides.forEach(slide => slide.style.transform = `translateX(-${slideCounter * 100}%)`)
+
+    slideNavigators.forEach((slideNavigator, index) => {
+
+        slideNavigator.style.backgroundColor = index == slideCounter ? '#66004d' : ''
+    })
 
 })
 
 btn3.addEventListener('click', function (e) {
 
-    if (e.target.className.slice(4, 8) == 'btn3') {
+    let slideCounter = e.target.className.slice(18, 19);
 
-        slides[0].style.transform = 'translateX(-200%)';
-        slides[2].style.transform = 'translateX(-200%)';
-        slides[1].style.transform = 'translateX(-200%)';
-        btn3.style.backgroundColor = '#66004d'
-        btn1.style.backgroundColor = '';
-        btn2.style.backgroundColor = '';
+    slides.forEach(slide => slide.style.transform = `translateX(-${slideCounter * 100}%)`)
 
-    }
+    slideNavigators.forEach((slideNavigator, index) => {
+
+        slideNavigator.style.backgroundColor = index == slideCounter ? '#66004d' : ''
+    })
+
 })
-
-
