a5c74d72d482046f350680e81aab1df47c28c184
sijadkhan1678 <sijadkhan1678@gmail.com>
2023-07-11 18:53:28 +0500

    Added navigator activators

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 // Get DOM Elements
-const slides = document.querySelectorAll('.box');
+const slides = document.querySelectorAll('.slides');
 const btn1 = document.querySelector('.btn1')
 const btn2 = document.querySelector('.btn2')
 const btn3 = document.querySelector('.btn3')
@@ -11,6 +11,9 @@ btn1.addEventListener('click', function (e) {
     slides[2].style.transform = 'translateX(0%)';
     slides[1].style.transform = 'translateX(0%)';
     slides[0].style.transform = 'translateX(0%)';
+    btn1.style.backgroundColor = '#66004d';
+    btn3.style.backgroundColor = ''
+    btn2.style.backgroundColor = '';
 
 })
 
@@ -19,15 +22,25 @@ btn2.addEventListener('click', function (e) {
     slides[0].style.transform = 'translateX(-100%)';
     slides[1].style.transform = 'translateX(-100%)';
     slides[2].style.transform = 'translateX(-100%)';
+    btn2.style.backgroundColor = '#66004d';
+    btn3.style.backgroundColor = ''
+    btn1.style.backgroundColor = '';
+        
 
 })
 
 btn3.addEventListener('click', function (e) {
-    
-     slides[0].style.transform = 'translateX(-200%)';
-    slides[2].style.transform = 'translateX(-200%)';
-    slides[1].style.transform = 'translateX(-200%)';
 
+    if (e.target.className.slice(4, 8) == 'btn3') {
+
+        slides[0].style.transform = 'translateX(-200%)';
+        slides[2].style.transform = 'translateX(-200%)';
+        slides[1].style.transform = 'translateX(-200%)';
+        btn3.style.backgroundColor = '#66004d'
+        btn1.style.backgroundColor = '';
+        btn2.style.backgroundColor = '';
+
+    }
 })
 
 
