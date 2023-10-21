28b4d1888c3c021ad8bd8a6cdcb2b906b11235f7
sijadkhan1678 <sijadkhan1678@gmail.com>
2023-10-21 16:36:07 +0500

    code optimzed used for loop intead two forEach loop

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const slideNavigators = document.querySelectorAll('.slideNavigator');
 let counter = 0
 // Detect slider to reach its last slide
 let is_Slider_Reached = false
+console.log("slides" + slides[0].style)
 
 
 function slide_Image(e = 1 || e) {
@@ -15,23 +16,37 @@ function slide_Image(e = 1 || e) {
     if (typeof e.target === 'object') {
 
         let slideCounter = e.target.className.slice(18, 19);
-        counter = slideCounter;
 
-        slides.forEach(slide => slide.style.transform = `translateX(-${slideCounter * 100}%)`)
+        let iterator = 0
 
-        slideNavigators.forEach((slideNavigator, index) => {
+        for (iterator; iterator < slides.length; iterator++) {
+            slides[iterator].style.transform = `translateX(-${slideCounter * 100}%)`
+            slideNavigators[iterator].style.backgroundColor = iterator == slideCounter ? '#66004d' : ''
+        }
 
-            slideNavigator.style.backgroundColor = index == slideCounter ? '#66004d' : ''
-        })
+        // slides.forEach(slide => slide.style.transform = `translateX(-${slideCounter * 100}%)`)
+
+        // slideNavigators.forEach((slideNavigator, index) => {
+
+        //     slideNavigator.style.backgroundColor = index == slideCounter ? '#66004d' : ''
+        // })
     }
     else {
+
         let slideCounter = e
-        slides.forEach(slide => slide.style.transform = `translateX(-${slideCounter * 100}%)`)
+        let iterator = 0
 
-        slideNavigators.forEach((slideNavigator, index) => {
+        for (iterator; iterator < slides.length; iterator++) {
 
-            slideNavigator.style.backgroundColor = index == slideCounter ? '#66004d' : ''
-        })
+            slides[iterator].style.transform = `translateX(-${slideCounter * 100}%)`
+            slideNavigators[iterator].style.backgroundColor = iterator == slideCounter ? '#66004d' : ''
+        }
+        // slides.forEach(slide => slide.style.transform = `translateX(-${slideCounter * 100}%)`)
+
+        // slideNavigators.forEach((slideNavigator, index) => {
+
+        //     slideNavigator.style.backgroundColor = index == slideCounter ? '#66004d' : ''
+        // })
     }
 
 
@@ -60,7 +75,7 @@ function slide_Automator() {
 }
 
 // Automate sliding
-setInterval(slide_Automator, 1500)
+setInterval(slide_Automator, 2500)
 
 // Adding DOM Event listener
 btn1.addEventListener('click', slide_Image)
